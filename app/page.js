ff8db6c24333c832e6cd77e0400c322bd4d3dc5d
Maic Gonzalez <miguel.gonzalez@gmail.com>
2024-10-30 15:27:50 -0700

    Added components to Page

--- app/page.js
+++ app/page.js
@@ -6,6 +6,9 @@ import TextBlock from "./components/TextBlock";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import ImageGallery from "./components/ImageGallery";
+import SplitBlock from "./components/SplitBlock";
+import CardSection from "./components/CardSection";
+import Teaser from "./components/Teaser";
 
 export default function Home({ params }) {
   const [entry, setEntry] = useState({});
@@ -16,7 +19,7 @@ export default function Home({ params }) {
       "home_page",
       ["hero_banner", "page_content.image_gallery.gallery_item.page"]
     );
-    // console.log("homepage:", entry[0][0]);
+    console.log("homepage:", entry[0][0]);
     setEntry(entry[0][0]);
     SetLoading(false);
   };
@@ -37,10 +40,24 @@ export default function Home({ params }) {
 
       {entry.page_content?.map((item, index) => {
         if (item.hasOwnProperty("text_block")) {
+          console.log(entry.page_content[index])
           return <TextBlock key={index} content={item.text_block} />;
         }
         if (item.hasOwnProperty("image_gallery")) {
+          console.log(entry.page_content[index])
           return <ImageGallery key={index} content={item.image_gallery} />;
+        }
+        if (item.hasOwnProperty("split_block")) {
+          console.log(entry.page_content[index])
+          return <SplitBlock key={index} content={item.split_block} />;
+        }
+        if (item.hasOwnProperty("card_collection")) {
+          console.log(entry.page_content[index])
+          return <CardSection key={index} content={item.card_collection} />;
+        }
+        if (item.hasOwnProperty("teaser")) {
+          console.log(entry.page_content[index])
+          return <Teaser content={item.teaser} key={index}  />;
         }
       })}
 
